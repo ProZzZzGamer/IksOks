6d4ea1650c4e780350a33fbaff49123311e51b28
ProZzZz_Gamer <amiinn.lulicami878@gmail.com>
2022-08-09 13:58:05 +0200

    2. korak gotov (unos polja za XO)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+
+        Scanner unos = new Scanner(System.in);
+        String mreza = unos.next();
+        int index = 0;
+        char mreza2;
+
+        while (index < 9) {
+            mreza2 = mreza.charAt(index);
+            if (!(mreza2 == 'X' || mreza2 == 'O' || mreza2 == '_') || mreza.length() != 9) {
+                System.out.println("Hazim kae ponovo");
+                mreza = unos.next();
+                index = 0;
+            }
+            index++;
+        }
+
+        String dzajinaMreza = mreza.substring(0,3);
+        String hazimovaMreza = mreza.substring(3, 6);
+        String mamarinaMreza = mreza.substring(6, 9);
+
+        System.out.println("-------");
+        System.out.println("| " + dzajinaMreza + " |");
+        System.out.println("| " + hazimovaMreza + " |");
+        System.out.println("| " + mamarinaMreza + " |");
+        System.out.println("-------");
     }
 }
