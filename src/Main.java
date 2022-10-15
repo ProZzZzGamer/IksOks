913a012def541dd593adac6264f56933f6175c15
prozzzzdzajinunuk <amiinn.lulicami878@gmail.com>
2022-10-15 10:58:28 +0200

    Promjenje: - program pregledava da li polje ima 9 karaktera i ima li karaktere 'X', 'O' i '_'; - novi ispis polja; - kombinacije za 'X' napravljene (kada je igra dobro postavljena, kad nije, kad je 'X' pobjedio).
    Stvari koje ostaju za 1.0 verziju:
    - kombinacije za 'O'.
    
    Unapređena će biti i nakon 1.0 verzije.

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,120 @@
 import java.util.Scanner;
 
 public class Main {
+    public Main() {
+    }
+
     public static void main(String[] args) {
-
         Scanner unos = new Scanner(System.in);
-        String mreza = unos.next();
-        int index = 0;
-        char mreza2;
+        char[][] nizPolje = new char[3][3];
+        String unosPolje = unos.nextLine();
+        int z = 0;
+        int x = 0;
+        int y = 0;
+        int l = 0;
+        int m = 0;
+        int k = 0;
+        int a = 0;
+        int s = 0;
+        int r = 0;
 
-        while (index < 9) {
-            mreza2 = mreza.charAt(index);
-            if (!(mreza2 == 'X' || mreza2 == 'O' || mreza2 == '_') || mreza.length() != 9) {
-                System.out.println("Hazim kae ponovo");
-                mreza = unos.next();
-                index = 0;
-            }
-            index++;
+
+        int c;
+        for (c = 0; unosPolje.length() != 9; unosPolje = unos.nextLine()) {
+            System.out.println("Unesi ponovo!");
         }
 
-        String dzajinaMreza = mreza.substring(0,3);
-        String hazimovaMreza = mreza.substring(3, 6);
-        String mamarinaMreza = mreza.substring(6, 9);
+        int d;
+        int e;
+        for (d = 0; d < 9; ++d) {
+            e = unosPolje.charAt(d);
+            if (e != 'X' && e != 'O' && e != '_') {
+                System.out.println("Igra nije validna (nisu unešeni validni karakteri 'X' 'O' '_')!");
+                unosPolje = unos.nextLine();
+                d = 0;
+            }
+        }
 
-        System.out.println("-------");
-        System.out.println("| " + dzajinaMreza + " |");
-        System.out.println("| " + hazimovaMreza + " |");
-        System.out.println("| " + mamarinaMreza + " |");
-        System.out.println("-------");
+        for (d = 0; d < nizPolje.length; d++) {
+            for (e = 0; e < nizPolje.length; e++) {
+                nizPolje[d][e] = unosPolje.charAt(c);
+                c++;
+            }
+        }
+
+        for (char[] i : nizPolje) {
+            for (char o : i) {
+                System.out.print(o + " ");
+            }
+            System.out.print("\n");
+        }
+
+        for (d = 0; d < nizPolje.length; ++d) {
+            for (e = 0; e < nizPolje.length; ++e) {
+                if (d == e && nizPolje[d][e] == 'X') {
+                    x++;
+                    if (x == 3) {
+                        y++;
+                    }
+                }
+
+                if (nizPolje.length - 1 - d == e && nizPolje[d][e] == 'X') {
+                    z++;
+                    if (z == 3) {
+                        y++;
+                    }
+                }
+
+                if (e == 0 && nizPolje[d][e] == 'X') {
+                    l++;
+                    if (l == 3) {
+                        y++;
+                    }
+                }
+
+                if (e == 1 && nizPolje[d][e] == 'X') {
+                    m++;
+                    if (m == 3) {
+                        y++;
+                    }
+                }
+
+                if (e == 2 && nizPolje[d][e] == 'X') {
+                    k++;
+                    if (k == 3) {
+                        y++;
+                    }
+                }
+
+                if (d == 0 && nizPolje[d][e] == 'X') {
+                    a++;
+                    if (a == 3) {
+                        y++;
+                    }
+                }
+
+                if (d == 1 && nizPolje[d][e] == 'X') {
+                    s++;
+                    if (s == 3) {
+                        y++;
+                    }
+                }
+                if (d == 2 && nizPolje[d][e] == 'X') {
+                    r++;
+                    if (r == 3) {
+                        y++;
+                    }
+                }
+            }
+        }
+
+        if (y > 1) {
+            System.out.println("Igra nije dobro postavljena.");
+        } else if (y == 1) {
+            System.out.println("X je dobio.");
+        } else if (y == 0) {
+            System.out.println("Nema pobjednika.");
+        }
+
     }
 }
